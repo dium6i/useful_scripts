d100c67e7ab6bcade24dff02166583df5b0da5c4
dium6i <75460093+dium6i@users.noreply.github.com>
2023-12-15 17:25:38 +0800

    Create 02_find_specific_labels.py

---
+++ scripts/02_find_specific_labels.py
@@ -0,0 +1,70 @@
+'''
+Author: Wei Qin
+Date: 2023-12-15
+Description:
+    Find images with specific labels in VOC dataset. 
+Update Log:
+    2023-12-15: File created.
+
+'''
+
+import os
+import xml.etree.ElementTree as ET
+
+
+data = 'path/of/voc/dataset'
+imgs = os.path.join(data, 'JPEGImages')
+xmls = os.path.join(data, 'Annotations')
+
+finds = ['cat']  # Labels hope to find.
+i = 0
+f = 10  # Number of images hope to find.
+found = []
+
+skip = False
+copy = True  # Copy found images to other directory.
+save_dir = 'path/of/saving'
+
+for img in os.listdir(imgs):
+    if i == f:  # Found enough images.
+        break
+
+    base_name = '.'.join(img.split('.')[:-1])
+    xml_path = os.path.join(xmls, base_name + '.xml')
+    if not os.path.exists(xml_path):
+        continue
+
+    tree = ET.parse(xml_path)
+    root = tree.getroot()
+
+    for obj in root.iter('object'):
+        if skip:  # Current image has been counted.
+            continue
+
+        if obj.find('name').text in finds:
+            i += 1
+            found.append(img)
+            skip = True  # Skip current image.
+
+            print(base_name)
+
+    skip = False
+
+if copy:
+    import shutil
+
+    copied_imgs = os.path.join(save_dir, 'JPEGImages')
+    if not os.path.exists(copied_imgs):
+        os.makedirs(copied_imgs)
+    copied_xmls = os.path.join(save_dir, 'Annotations')
+    if not os.path.exists(copied_xmls):
+        os.makedirs(copied_xmls)
+
+    for img in found:
+        base_name = '.'.join(img.split('.')[:-1])
+        xml_path = os.path.join(xmls, base_name + '.xml')
+
+        shutil.copyfile(os.path.join(imgs, img), 
+            os.path.join(copied_imgs, img))
+        shutil.copyfile(xml_path,
+            os.path.join(copied_xmls, base_name + '.xml'))
