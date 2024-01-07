2d33ca6ce0723bdb8fab45780b27fc7468f34d6d
dium6i <75460093+dium6i@users.noreply.github.com>
2024-01-07 12:59:00 +0800

    Update 03_crop_voc_images.py

--- scripts/03_crop_voc_images.py
+++ scripts/03_crop_voc_images.py
@@ -2,58 +2,57 @@
 Author: Wei Qin
 Date: 2023-12-15
 Description:
-    Crop images(VOC dataset) according to labels.
+    Crop images(VOC dataset) according to annotations
+    to check whether the label is wrong.
 Update Log:
     2023-12-15: File created.
+    2024-01-07: Use multi-threading to speed up.
 
 '''
 
+from concurrent.futures import ThreadPoolExecutor, as_completed
 import os
+import time
 import xml.etree.ElementTree as ET
 
 import cv2
 from tqdm import tqdm
 
 
-data = 'path/of/voc/dataset'
-imgs = os.path.join(data, 'JPEGImages')
-xmls = os.path.join(data, 'Annotations')
-crop = os.path.join(data, 'croped')
+def process_image(params, img):
+    '''
+    Single image cropping process.
 
-crop_all = False  # Weather to crop all labels.
-croped_labels = [  # Labels need to be croped.
-    'cat',
-    'dog',
-    'people',
-    'kite'
-]
+    Args:
+        params (dict): Parameters.
+        img (string): Image file name like "sample.jpg".
 
-for img in tqdm(os.listdir(imgs), desc="Cropping"):
+    Returns:
+        None.
+    '''
     snippet = img.split('.')
     base = '.'.join(snippet[:-1])
     ext = snippet[-1]
 
-    img_path = os.path.join(imgs, img)
-    xml_path = os.path.join(xmls, base + '.xml')
+    img_path = os.path.join(params['imgs_dir'], img)
+    xml_path = os.path.join(params['xmls_dir'], base + '.xml')
+
+    im = cv2.imread(img_path)
+    i = 1  # Starting index of cropped images.
 
     tree = ET.parse(xml_path)
     root = tree.getroot()
 
-    im = cv2.imread(img_path)
-    i = 1 # Starting index of cropped images. 
-
     for obj in root.iter('object'):
         label = obj.find('name').text
 
-        if not crop_all and label not in croped_labels:
+        if not params['crop_all'] and label not in params['cropped_labels']:
             continue
 
-        # Create folder based on label. 
-        obj_dir = os.path.join(crop, label)
+        obj_dir = os.path.join(params['save_dir'], label)
         if not os.path.exists(obj_dir):
             os.makedirs(obj_dir)
 
-        # Crop labels. 
         bndbox = obj.find('bndbox')
         box = [
             int(float(bndbox.find('xmin').text)),
@@ -62,7 +61,49 @@ for img in tqdm(os.listdir(imgs), desc="Cropping"):
             int(float(bndbox.find('ymax').text))
         ]
         croped = im[box[1]:box[3], box[0]:box[2]]
-        
-        # Save cropped images. 
+
         cv2.imwrite(os.path.join(obj_dir, f'{base}_{i}.{ext}'), croped)
         i += 1
+
+
+def run(params):
+    '''
+    Main process.
+
+    Args:
+        params (dict): Parameters.
+
+    Returns:
+        None.
+    '''
+    t = time.strftime('%Y%m%d_%H%M%S')
+    params['imgs_dir'] = os.path.join(params['data'], params['imgs'])
+    params['xmls_dir'] = os.path.join(params['data'], params['xmls'])
+    params['save_dir'] = os.path.join(params['data'], params['save'], t)
+    imgs_list = os.listdir(params['imgs_dir'])
+
+    # Using ThreadPoolExecutor for multi-threading
+    with ThreadPoolExecutor() as executor:
+        futures = [executor.submit(process_image, params, img)
+                   for img in imgs_list]
+        for _ in tqdm(
+                as_completed(futures),
+                total=len(imgs_list),
+                desc='Cropping'):
+            pass
+
+    print('Cropping completed.')
+
+
+if __name__ == '__main__':
+    # Setting parameters
+    params = {
+        'data': 'path/of/dataset',  # Contains imgs and xmls
+        'imgs': 'JPEGImages',  # Folder of images
+        'xmls': 'Annotations',  # Folder of labels
+        'save': 'cropped',  # Folder to save cropped images
+        'crop_all': False,  # Whether to crop all labels
+        'cropped_labels': ['cat', 'dog']  # Labels need to be cropped
+    }
+
+    run(params)
