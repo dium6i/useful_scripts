1ead174ef31faf00d11a2f30d1343058768e036b
dium6i <75460093+dium6i@users.noreply.github.com>
2024-03-29 15:36:41 +0800

    Update 10_onnx_model_with_onnxruntime.py

--- scripts/10_onnx_model_with_onnxruntime.py
+++ scripts/10_onnx_model_with_onnxruntime.py
@@ -8,6 +8,7 @@ Update Log:
     2024-03-27: - Added functions: 
                     -- Whether to contain label name in the results.
                     -- Set inference threads.
+    2024-03-29: - Use cv2.dnn.NMSBoxes() to replace the customized NMS method.
 
 """
 
@@ -18,87 +19,6 @@ import cv2
 import numpy as np
 import onnxruntime as ort
 from tqdm import tqdm
-
-
-def non_max_suppression(boxes, scores, iou_threshold):
-    """
-    Perform Non-Maximum Suppression to filter out overlapping bounding boxes.
-
-    Args:
-    - boxes: List of bounding boxes in the format [x_min, y_min, x_max, y_max].
-    - scores: List of confidence scores corresponding to each bounding box.
-    - iou_threshold: Threshold for Intersection over Union.
-
-    Returns:
-    - keep_indices: List of indices to keep after NMS.
-    """
-    if len(boxes) == 0:
-        return []
-
-    # Sort boxes by their scores in descending order
-    sorted_indices = sorted(
-        range(len(scores)), key=lambda i: scores[i], reverse=True
-        )
-
-    keep_indices = []
-    while len(sorted_indices) > 0:
-        # Pick the box with the highest confidence score
-        max_index = sorted_indices[0]
-        keep_indices.append(max_index)
-
-        # Compute IoU between the picked box and all other boxes
-        selected_box = boxes[max_index]
-        other_boxes = [boxes[i] for i in sorted_indices[1:]]
-        iou_scores = [
-            calculate_iou(selected_box, other_box) 
-            for other_box in other_boxes
-            ]
-
-        # Filter out boxes with IoU greater than threshold
-        filtered_indices = [
-            i for i, iou in zip(sorted_indices[1:], iou_scores) 
-            if iou <= iou_threshold
-            ]
-        sorted_indices = filtered_indices
-
-    return keep_indices
-
-
-def calculate_iou(box1, box2):
-    """
-    Calculate the Intersection over Union (IoU) of two bounding boxes.
-
-    Args:
-    - box1: Bounding box in the format [x_min, y_min, x_max, y_max].
-    - box2: Bounding box in the format [x_min, y_min, x_max, y_max].
-
-    Returns:
-    - iou: Intersection over Union (IoU) score.
-    """
-    x1, y1, x2, y2 = box1
-    x3, y3, x4, y4 = box2
-
-    # Calculate the coordinates of the intersection rectangle
-    intersection_x1 = max(x1, x3)
-    intersection_y1 = max(y1, y3)
-    intersection_x2 = min(x2, x4)
-    intersection_y2 = min(y2, y4)
-
-    # Calculate intersection area
-    intersection_area = max(0, intersection_x2 - intersection_x1 + 1) * \
-                        max(0, intersection_y2 - intersection_y1 + 1)
-
-    # Calculate areas of each bounding box
-    box1_area = (x2 - x1 + 1) * (y2 - y1 + 1)
-    box2_area = (x4 - x3 + 1) * (y4 - y3 + 1)
-
-    # Calculate Union area
-    union_area = box1_area + box2_area - intersection_area
-
-    # Calculate IoU
-    iou = intersection_area / union_area
-
-    return iou
 
 
 def load_model(model_path, thread_num=1):
@@ -161,8 +81,8 @@ def postprocess(
 
     Returns:
     - results: Filtered results in the format 
-               [[class_id, score, xmin, ymin, xmax, ymax], ......] or
-               [[class_id, class_name, score, xmin, ymin, xmax, ymax], ......].
+               [[class_id, score, xmin, ymin, w, h], ......] or
+               [[class_id, class_name, score, xmin, ymin, w, h], ......].
     """
     outs = np.transpose(np.squeeze(outs))  # shape: (8400, 4 + labels number)
     boxes, scores, class_ids = [], [], []
@@ -181,14 +101,14 @@ def postprocess(
     x, y, w, h = outs[:, 0].copy(), outs[:, 1].copy(), outs[:, 2].copy(), outs[:, 3].copy()
     outs[:, 0] = (x - w / 2) * x_factor
     outs[:, 1] = (y - h / 2) * y_factor
-    outs[:, 2] = (x + w / 2) * x_factor
-    outs[:, 3] = (y + h / 2) * y_factor
+    outs[:, 2] = w * x_factor
+    outs[:, 3] = h * y_factor
     outs[:, :4] = outs[:, :4].astype(int)
 
     boxes = outs[:, :4]
     scores = outs[:, -1]
     class_ids = outs[:, -2]
-    indices = non_max_suppression(boxes, scores, iou)
+    indices = cv2.dnn.NMSBoxes(boxes, scores, conf, iou)
 
     if labels:
         results = [[int(class_ids[i]), 
