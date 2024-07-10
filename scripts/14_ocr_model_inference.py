6bf7a4543955fc672f22e77cbe8d296e74fe6470
dium6i <75460093+dium6i@users.noreply.github.com>
2024-07-10 11:27:08 +0800

    Update 14_ocr_model_inference.py

--- scripts/14_ocr_model_inference.py
+++ scripts/14_ocr_model_inference.py
@@ -3,11 +3,11 @@ Author: Wei Qin
 Date: 2024-06-03
 Description:
     PPOCR model inference and results visualization.
-    The current version is only suitable for model inference using FastDeploy.
 Update Log:
     2024-06-03: - File created.
     2024-06-07: - Optimized code structure.
     2024-06-26: - Optimized code structure.
+    2024-07-10: - Changed the inference engine from FastDeploy to RapidOCR.
 
 """
 
@@ -15,144 +15,201 @@ import os
 import time
 
 import cv2
-import fastdeploy as fd
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
+from rapidocr_onnxruntime import RapidOCR
+from tqdm import tqdm
 
 
-def ocr_model(img, model_path):
-    """
-    Initialize OCR model.
+class OCR:
+    def __init__(
+            self,
+            text_score=0.5,
+            det_model_path=None,
+            det_unclip_ratio=1.6,
+            rec_model_path=None):
+        """
+        RapidOCR has built-in general OCR models(PPOCRv4).
+        If costomized models need to be used, det_model_path and rec_model_path
+        should be specified.
+        """
+        self.engine = RapidOCR(
+            text_score=text_score,
+            det_model_path=det_model_path,
+            det_unclip_ratio=det_unclip_ratio,
+            rec_model_path=rec_model_path)
 
-    Args:
-        img (numpy.ndarray): Image array.
+    def model_infer(self, im):
+        """
+        Model inference and results postprocess.
 
-    Returns:
-        model_path (str): Model path.
-    """
-    det_option = fd.RuntimeOption()
-    rec_option = fd.RuntimeOption()
+        Args:
+            im (numpy.ndarray): Image array.
 
-    det_option.use_openvino_backend()
-    rec_option.use_openvino_backend()
+        Returns:
+            results (list): Inference results after postprocessing.
+            dt (float): Inference time.
+        """
+        t0 = time.time()
+        res, _ = self.engine(im, use_det=True, use_cls=False, use_rec=True)
+        dt = time.time() - t0
 
-    det_model = fd.vision.ocr.DBDetector(
-        model_file=os.path.join(model_path, 'det/inference.pdmodel'),
-        params_file=os.path.join(model_path, 'det/inference.pdiparams'),
-        runtime_option=det_option)
-    # det_model.postprocessor.det_db_unclip_ratio = 0.9
+        # Rearrange and filter res
+        results = []
+        for result in res:
+            bbox, text, score = result
+            box_wh = (bbox[1][0] - bbox[0][0], bbox[-1][1] - bbox[0][1])
+            results.append([bbox, box_wh, text, score])
 
-    rec_model = fd.vision.ocr.Recognizer(
-        model_file=os.path.join(model_path, 'rec/inference.pdmodel'),
-        params_file=os.path.join(model_path, 'rec/inference.pdiparams'),
-        label_path=os.path.join(model_path, 'ppocr_keys_v1.txt'),
-        runtime_option=rec_option)
+        if self.im_count == 1:
+            print(f'OCR Results:')
+            list(map(print, results))
+            print(f'Inference time: {dt * 1000:.2f} ms')
+            return results
+        else:
+            return results, dt
 
-    ocr_model = fd.vision.ocr.PPOCRv3(
-        det_model=det_model,
-        cls_model=None,
-        rec_model=rec_model)
+    def draw_boxes(self, im, results):
+        """
+        Visualize boxes based on filtered results.
 
-    t1 = time.time()
-    results = ocr_model.predict(img)
-    print(f'Prediction time: {(time.time() - t1) * 1000:.2f} ms')
+        Args:
+            im (numpy.ndarray): Image array.
+            results (list): Filtered results.
 
-    return results
+        Returns:
+            im (numpy.ndarray): Visualized image.
+        """
+        colorset = [ # RGB
+            (218, 179, 218), (138, 196, 208), (112, 112, 181), (255, 160, 100), 
+            (106, 161, 115), (232, 190,  93), (211, 132, 252), ( 77, 190, 238), 
+            (  0, 170, 128), (196, 100, 132), (153, 153, 153), (194, 194,  99), 
+            ( 74, 134, 255), (205, 110,  70), ( 93,  93, 135), (140, 160,  77), 
+            (255, 185, 155), (255, 107, 112), (165, 103, 190), (202, 202, 202), 
+            (  0, 114, 189), ( 85, 170, 128), ( 60, 106, 117), (250, 118, 153), 
+            (119, 172,  48), (171, 229, 232), (160,  85, 100), (223, 128,  83), 
+            (217, 134, 177), (133, 111, 102), 
+        ]
 
+        h, w, _ = im.shape
+        # create a white image to display results
+        white_im = Image.new('RGB', (w, h), (255, 255, 255))
+        draw = ImageDraw.Draw(white_im)
 
-def draw_bbox(img, idx, bbox, alpha=0.5):
-    """
-    Draw a bounding box with a transparent fill on an image.
+        # Visualize OCR results
+        for i, res in enumerate(results):
+            bbox, wh, text, score = res
 
-    Args:
-        img (numpy.ndarray): Array of original image.
-        idx (int): Index of bbox.
-        bbox (list): List of four corner points of the bounding box [(x1, y1), (x2, y2), (x3, y3), (x4, y4)].
-        alpha (float): The transparency factor of the fill. (0 is fully transparent)
+            # Draw box on original image
+            color = colorset[i % len(colorset)][::-1]
+            overlay = im.copy()
+            # convert bbox and reshape for cv2 functions
+            points = np.array(bbox, np.int32).reshape((-1, 1, 2))
+            # draw box on characters
+            cv2.fillPoly(overlay, [points], color)
+            # blend images for transparent box
+            cv2.addWeighted(overlay, 0.5, im, 0.5, 0, im)
+            # draw outline of box
+            cv2.polylines(im, [points], isClosed=True, color=color, thickness=1)
 
-    Returns:
-        output (numpy.ndarray): Array of image with bbox drawn.
-    """
-    colorset = [ # RGB
-        (218, 179, 218), (138, 196, 208), (112, 112, 181), (255, 160, 100), 
-        (106, 161, 115), (232, 190,  93), (211, 132, 252), ( 77, 190, 238), 
-        (  0, 170, 128), (196, 100, 132), (153, 153, 153), (194, 194,  99), 
-        ( 74, 134, 255), (205, 110,  70), ( 93,  93, 135), (140, 160,  77), 
-        (255, 185, 155), (255, 107, 112), (165, 103, 190), (202, 202, 202), 
-        (  0, 114, 189), ( 85, 170, 128), ( 60, 106, 117), (250, 118, 153), 
-        (119, 172,  48), (171, 229, 232), (160,  85, 100), (223, 128,  83), 
-        (217, 134, 177), (133, 111, 102), 
-    ]
-    color = colorset[idx % len(colorset)]
-    color = color[::-1]  # BGR to RGB
+            # Draw text on result image
+            font = ImageFont.truetype(self.font_path, size=wh[1])
+            draw.text(bbox[0], text, fill=color, font=font)
 
-    overlay = img.copy()
-    output = img.copy()
+        white_im = np.array(white_im)
+        vis_img = cv2.hconcat([im, white_im])
 
-    # Convert bbox to numpy array and reshape for cv2 functions
-    points = np.array(bbox, np.int32).reshape((-1, 1, 2))
+        return vis_img
 
-    # Fill the polygon
-    cv2.fillPoly(overlay, [points], color)
+    def save_visualization(self, im, results):
+        """
+        Create a save folder in the directory where the current image file is
+        located, and save the visualized images. When input is numpy.ndarray,
+        set the code file as an image file.
 
-    # Blend the overlay with the original image
-    cv2.addWeighted(overlay, alpha, output, 1 - alpha, 0, output)
+        Args:
+            im (numpy.ndarray): Image array.
+            results (list): Inference results after postprocessing.
 
-    # Draw the outline of the bounding box
-    cv2.polylines(output, [points], isClosed=True, color=color, thickness=1)
+        """
+        if self.visualize:
+            if not self.save_dir:
+                # os.path.abspath(__file__) is the path of current code file.
+                current_file = self.img_path if self.img_path else os.path.abspath(__file__)
+                current_file_dir = os.path.dirname(current_file)
+                self.save_dir = os.path.join(current_file_dir, 'visualized')
+                os.makedirs(self.save_dir, exist_ok=True)
 
-    return output, color
+            save_name = os.path.basename(self.img_path) if self.img_path else 'visualized.jpg'
+            save_path = os.path.join(self.save_dir, save_name)
+            cv2.imwrite(save_path, self.draw_boxes(im, results))
+            if self.im_count == 1:
+                print(f'Visualized result saved at: {save_path}')
 
+    def predict(self, im, save_dir=None, visualize=False, font_path=None):
+        """
+        Run model inference on the input image.
 
-def visualize_results(im, ocr_results, font_path, img_path):
-    """
-    Visualize the OCR results.
+        Args:
+            im (np.ndarray|str): Image array or image(s) directory.
 
-    Args:
-        im (numpy.ndarray): Array of original image.
-        ocr_results (list): OCR results.
-        font_path (str): Path of the font used for visualization.
-        img_path (str): Path of the source image.
+        Returns:
+            results (list): Inference results after postprocessing.
+        """
+        self.save_dir = save_dir
+        if self.save_dir and os.path.isfile(save_dir):
+            self.save_dir = os.path.join(os.path.dirname(save_dir), 'visualized')
+            os.makedirs(self.save_dir, exist_ok=True)
+        self.visualize = visualize
+        self.font_path = font_path
 
-    Returns:
-        None.
-    """
-    h, w, _ = im.shape
-    result_im = Image.new('RGB', (w, h), (255, 255, 255))
-    draw = ImageDraw.Draw(result_im)
+        # Numpy array as input
+        if isinstance(im, np.ndarray):
+            self.im_count = 1
+            self.img_path = False
+            results = self.model_infer(im)
+            self.save_visualization(im, results)
 
-    # Visualize OCR results
-    for i, res in enumerate(ocr_results):
-        bbox, wh, text, score = res
-        im, color = draw_bbox(im, i, bbox)
-        font = ImageFont.truetype(font_path, size=wh[1])
-        draw.text(bbox[-1], text, fill=color, font=font)
+        # Path as input
+        elif isinstance(im, str):
+            if os.path.isfile(im):  # Single image
+                self.im_count = 1
+                self.img_path = im
+                im_array = cv2.imread(im)
+                results = self.model_infer(im_array)
+                self.save_visualization(im_array, results)
 
-    result_img = np.array(result_im)
-    vis_img = cv2.hconcat([im, result_img])
+            else:  # Directory of images
+                self.im_count = -1
+                t = 0
+                img_list = [i for i in os.listdir(im) 
+                    if os.path.isfile(os.path.join(im, i))]
+                for img in tqdm(img_list, total=len(img_list), desc='Processing'):
+                    img_path = os.path.join(im, img)
+                    self.img_path = img_path
+                    im_array = cv2.imread(img_path)
+                    results, dt = self.model_infer(im_array)
+                    self.save_visualization(im_array, results)
+                    t += dt
 
-    # Save Visualization results
-    dirname = os.path.dirname(img_path)
-    base, ext = os.path.splitext(os.path.basename(img_path))
-    save_path = os.path.join(dirname, base + '_vis.jpg')
-    cv2.imwrite(save_path, vis_img)
-    print(f'Visualized image saved at: {save_path}')
+                print(f'Inference time:\n    Total: {t * 1000:.2f} ms')
+                print(f'    Avg: {t * 1000 / len(img_list):.2f} ms')
+                print(f'Visualized results saved at: {self.save_dir}')
+                results = None
+
+        # Unsupported input
+        else:
+            print('Please double check your input.')
+            results = None
+
+        return results
 
 
 if __name__ == '__main__':
-    font_path = 'path/of/font'
-    img_path = 'path/of/image'
-    im = cv2.imread(img_path)
-
-    # Model inference
-    model_path = 'path/of/models/directory'
-    results = ocr_model(im, model_path)
-
-    # Rearrange and filter results
-    ocr_results = [[list(zip(results.boxes[i][0::2], results.boxes[i][1::2])),  # boxes
-                    (results.boxes[i][2] - results.boxes[i][0], results.boxes[i][5] - results.boxes[i][3]),  # boxes w and h
-                    results.text[i],  # text
-                    round(results.rec_scores[i], 4)]  # confidence
-                   for i, _ in enumerate(results.boxes) if results.rec_scores[i] > 0.5]
-
-    visualize_results(im, ocr_results, font_path, img_path)
+    model = OCR(det_unclip_ratio=2.0)
+    model.predict(
+        'path/of/image/directory',  # Image file or directory
+        save_dir='path/to/save',  # Path to save visualized results
+        visualize=True, 
+        font_path='path/of/font',  # Path of ttf font, required if visualize
+    )
